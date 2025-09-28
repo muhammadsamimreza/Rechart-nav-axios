4ffd3e20595b67f2ea235f2c8891cb372fa1b60c
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-09-28 15:24:28 +0600

    use axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import StudentMarksChart from './components/StudentMarksChart/StudentMarksChart'
+import axios from "axios";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import StudentMarksChart from "./components/StudentMarksChart/StudentMarksChart";
+import { Suspense } from "react";
 
-
+const studentsPromise = axios("students.json");
 function App() {
- 
-
   return (
     <div>
       <Navbar></Navbar>
-      <StudentMarksChart></StudentMarksChart>
+      <Suspense fallback={<h1>Loading.........</h1>}>
+        <StudentMarksChart
+          studentsPromise={studentsPromise}
+        ></StudentMarksChart>
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/StudentMarksChart/StudentMarksChart.jsx
+++ src/components/StudentMarksChart/StudentMarksChart.jsx
@@ -1,5 +1,5 @@
 import { ScrollText } from "lucide-react";
-import React from "react";
+import React, { use } from "react";
 import {
   Bar,
   BarChart,
@@ -46,7 +46,11 @@ const students = [
   },
 ];
 
-const StudentMarksChart = () => {
+const StudentMarksChart = ({ studentsPromise }) => {
+  const studentsArray = use(studentsPromise);
+  const studentData = studentsArray.data;
+  console.log(studentData);
+
   return (
     <div className="my-c py-10">
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
@@ -136,6 +140,95 @@ const StudentMarksChart = () => {
                 <Bar dataKey={"chemistry"} fill="blue" barSize={20}></Bar>
                 <Bar dataKey={"math"} fill="tomato" barSize={20}></Bar>
               </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+        <div className="w-full">
+          <div className="flex items-center gap-3 text-teal-600 px-2 mb-5">
+            <span className="text-blue-600">
+              <ScrollText></ScrollText>
+            </span>
+            <h1 className="md:text-3xl font-semibold">
+              Final Year Result Chart (Bar)
+            </h1>
+          </div>
+          <div className="h-[300px] md:h-[300px] px-5 mt-30">
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart data={studentData}>
+                <CartesianAxis stroke=""></CartesianAxis>
+                <XAxis
+                  dataKey="name"
+                  stroke="teal"
+                  tick={{ fontSize: 12 }}
+                ></XAxis>
+                <YAxis></YAxis>
+
+                <Legend
+                  width={100}
+                  wrapperStyle={{
+                    width: 130,
+                    top: -100,
+                    left: 70,
+                    backgroundColor: "#f5f5f5",
+                    border: "1px solid #d5d5d5",
+                    borderRadius: 3,
+                    lineHeight: "20px",
+                    padding: "2px",
+                  }}
+                />
+                <Tooltip></Tooltip>
+                <Bar dataKey={"physics"} fill="OrangeRed" barSize={20}></Bar>
+                <Bar dataKey={"chemistry"} fill="black" barSize={20}></Bar>
+                <Bar dataKey={"math"} fill="Goldenrod" barSize={20}></Bar>
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+        {/* Second Chart */}
+        <div className="w-full">
+          <div className="flex items-center gap-3 text-green-500 px-2 mb-5">
+            <span className="text-blue-600">
+              <ScrollText></ScrollText>
+            </span>
+            <h1 className="md:text-3xl font-semibold">
+              Final Year Result Chart (Line)
+            </h1>
+          </div>
+          <div className="h-[300px] md:h-[300px] px-5 mt-0 md:mt-20">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart data={studentData}>
+                <CartesianGrid
+                  stroke="teal"
+                  strokeDasharray="5 5"
+                ></CartesianGrid>
+
+                <Line
+                  type="monotone"
+                  strokeWidth={1}
+                  dataKey="physics"
+                  stroke="tomato"
+                ></Line>
+                <Line
+                  type="monotone"
+                  strokeWidth={1}
+                  dataKey="chemistry"
+                  stroke="blue"
+                ></Line>
+                <Line
+                  type="monotone"
+                  strokeWidth={1}
+                  dataKey="math"
+                  stroke="black"
+                ></Line>
+                <XAxis dataKey="name" tick={{ fontSize: 12 }}></XAxis>
+                <YAxis></YAxis>
+                <Legend></Legend>
+                <Legend></Legend>
+                <Legend></Legend>
+                <Tooltip></Tooltip>
+              </LineChart>
             </ResponsiveContainer>
           </div>
         </div>
